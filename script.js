a0d504b0764dc411aeeae49eeb8ee605207e6ab5
Kazi Arham <210149902@aston.ac.uk>
2023-06-14 18:55:21 +0100

    Add Robot Card functionality working

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ const btnMakeRobot = document.querySelector("#btnMakeRobot");
 // RobotCard class for creating an object
 class RobotCard {
     // Constructing the Person object
-    constructor(parentElement) {
+    constructor(parentElement, name, picture) {
         this.parentElement = parentElement;
+        this.name = name;
+        this.picture = picture;
     }
 
     // This method creates a new robot card and renders data
@@ -19,17 +21,31 @@ class RobotCard {
 
         // Creates an image
         let image = document.createElement("img");
-        image.src = "robots/2023-05-28_12h31_34.png";
+        image.src = "robots/" + robotImage.files[0].name;
         image.classList.add("card-img-top", "py-2");
         image.alt = "Robot picture";
 
+        // Creates a card body
+        let cardBody = document.createElement("div");
+        cardBody.classList.add("card-body");
+
+        // Creates a name
+        let robotName = document.createElement("h5");
+        robotName.classList.add("card-title", "robotName", "text-center");
+        robotName.innerHTML = this.name.toUpperCase();
+
+        cardBody.appendChild(robotName);
+
         card.appendChild(image);
+        card.appendChild(cardBody);
         this.parentElement.appendChild(card);
     }    
 }
 
 // Button to create a robot card
 btnMakeRobot.addEventListener("click", function () {
-    const newRoboCard = new RobotCard(parentElement);
+    const newRoboCard = new RobotCard(parentElement, setName.value, robotImage);
     newRoboCard.createRobotCard();
+    setName.value = "";
+    robotImage.value = "";
 });
